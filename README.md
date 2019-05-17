eb278d888c74a34c554b72981b95b6e01be356c9
zhahuilin <chahuilin@zhexinit.com>
2019-05-17 15:40:06 +0800

    增加spring-boot-devtools

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 spring boot和tk-mybatis集成
 
 支持多数据源
+
+
+#spring-boot-devtools实现热部署
+
+1.找到idea的Preferences -> Build, Execution, Deployment -> Compiler，勾选Build project automatically
+
+2.回到idea正常界面，Mac使用快捷键shift+option+command+/，window上的快捷键是Shift+Ctrl+Alt+/，打开Registry，勾选
+compiler.automake.allow.when.app.runningcompiler.automake.allow.when.app.running
+通过以上的设置就可以在不重启服务的情况下加载html，但如果修改java文件，服务在几秒后会自动重启，如果不希望服务重启需要在application.properties或application.yml中添加spring.devtools.reatart.enable=false
--- src/main/java/com/example/tkmybatisdemo/web/AgentController.java
+++ src/main/java/com/example/tkmybatisdemo/web/AgentController.java
@@ -5,6 +5,7 @@ import com.example.tkmybatisdemo.entity.Agent;
 import com.example.tkmybatisdemo.mapper.opensips.SubscriberMapper;
 import com.example.tkmybatisdemo.mapper.voipplatform.AgentMapper;
 import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.UUID;
 
 @RequestMapping("agent")
 @RestController
+@Slf4j
 public class AgentController {
     @Resource
     private AgentMapper agentMapper;
@@ -24,6 +26,8 @@ public class AgentController {
 
     @GetMapping("{id}")
     public Object findById(@PathVariable Integer id) {
+        log.info("popo");
+        log.info("test");
         return agentMapper.selectByPrimaryKey(id);
     }
 
